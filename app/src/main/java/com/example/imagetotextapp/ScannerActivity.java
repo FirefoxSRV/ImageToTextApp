f93d8ff7189c34b2f1b68b51cf725790b01a82e8
FirefoxSRV <shreyasvisweshwaran@gmail.com>
2023-09-12 00:44:58 +0530

    Tiny UI changes - App works well

--- app/src/main/java/com/example/imagetotextapp/ScannerActivity.java
+++ app/src/main/java/com/example/imagetotextapp/ScannerActivity.java
@@ -1,23 +1,151 @@
 package com.example.imagetotextapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import android.Manifest;
+import android.app.DownloadManager;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.Text;
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
 
 public class ScannerActivity extends AppCompatActivity {
 
     private ImageView captureImage;
     private TextView resultText;
-    private Button takePhoto, detectText;
+    private Button takePhoto, detectTextButton;
+    private Bitmap imageBitmap;
 
-
-
+    static final int REQUEST_IMAGE_CAPTURE =1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_scanner);
+
+        captureImage = findViewById(R.id.inputImage);
+        takePhoto = findViewById(R.id.buttonClickPicture);
+        resultText = findViewById(R.id.textOutput);
+
+        detectTextButton = findViewById(R.id.buttonDetectText);
+
+        detectTextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DetectTextCall();
+            }
+        });
+
+        takePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                 if (isPermissionGranted()){
+
+                     TakePic();
+                 }else{
+                     RequestCameraAccess();
+                 }
+            }
+        });
+
+    }
+
+    private void TakePic() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // If device has a camera
+        if (getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(grantResults.length > 0){
+            boolean cameraPermission = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+            if(cameraPermission){
+                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+                TakePic();
+            }else{
+                Toast.makeText(getApplicationContext(), "Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+            Bundle extras = data.getExtras();
+            imageBitmap = (Bitmap) extras.get("data");
+            captureImage.setImageBitmap((imageBitmap));
+        }
+    }
+
+    private void RequestCameraAccess() {
+        int PERMISSION_CODE = 200;
+        ActivityCompat.requestPermissions(this,new String[]{
+                Manifest.permission.CAMERA
+        },PERMISSION_CODE);
+    }
+
+    private boolean isPermissionGranted() {
+        return PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(getApplicationContext(),Manifest.permission.CAMERA);
+    }
+
+    private void DetectTextCall(){
+        InputImage image = InputImage.fromBitmap(imageBitmap,0);
+        TextRecognizer recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
+        Task<Text> result = recognizer.process(image).addOnSuccessListener(new OnSuccessListener<Text>() {
+            @Override
+            public void onSuccess(Text text) {
+                StringBuilder result = new StringBuilder();
+                for(Text.TextBlock block : text.getTextBlocks()){
+                    String blockText = block.getText();
+                    Point[] blockCornerPoint = block.getCornerPoints();
+                    Rect blockFrame = block.getBoundingBox();
+                    for (Text.Line line : block.getLines()){
+                        String lineText = line.getText();
+                        Point[] lineCornerPoint = line.getCornerPoints();
+                        Rect lineRect = line.getBoundingBox();
+                        for(Text.Element element : line.getElements()){
+                            String elementText = element.getText();
+                            result.append(elementText);
+                        }
+                        resultText.setText(blockText);
+                    }
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(), "Failed to detect text from image", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
